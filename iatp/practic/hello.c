f6be7dc63bb810f6060ce0a59368b0113bb2f4cd
mend0za <mend0za>
2008-02-11 00:24:05 +0000

    save IATP-2007 works

---
+++ iatp/practic/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char ** argv)
+{
+	char *p=(char *)13;
+	double x=0.5;
+	
+	printf("Hello, world\n");
+	//printf("Oops, i did it again %s\n", p);
+	printf("Natural logarithm of %f is %f\n", x, log(x) );	
+
+	return 0;
+
+}
